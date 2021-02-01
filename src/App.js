f5d7d564fba948c164bf02cf5269de2d4a8aa49d
Tommy Danielsson <tdserviceco@gmail.com>
2021-02-01 16:50:48 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, {useEffect} from 'react';
+
+function App() {
+  useEffect(() => {
+    document.title = "Welcome"
+ }, []);
+
+  return (
+   <>
+   </>
+  )
+}
+
+export default App;
